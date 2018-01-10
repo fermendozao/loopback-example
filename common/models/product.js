b8b5b9a52d448bde701a8472b1190eb5714b2963
Fernando Mendoza <goliat8903@gmail.com>
2018-01-09 18:55:54 -0600

    Add buy remote method for Product model

--- common/models/product.js
+++ common/models/product.js
@@ -1,5 +1,23 @@
 'use strict';
 
 module.exports = function(Product) {
+  /**
+   * Buy this product
+   * @param {number} quantity Number of products to buy
+   * @param {Function(Error, object)} callback
+   */
+
+  const validQuantity = quantity => Boolean(quantity > 0);
+
+  Product.prototype.buy = function(quantity, callback) {
+    if (!validQuantity(quantity)) {
+      return callback(`Invalid quantity ${quantity}`);
+    }
+
+    const result = {
+      status: `You bought ${quantity} product(s)`,
+    };
+    callback(null, result);
+  };
 
 };
